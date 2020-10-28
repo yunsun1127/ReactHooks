da778ebfd128fd03c8be47d483337747de746706
yunsun1127 <yunsun1127@naver.com>
2020-10-28 19:19:43 +0900

    make ClassComp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import FuncComp from "./FuncComp";
+import ClassComp from "./ClassComp";
 
 function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-
       <FuncComp initNumber={2} />
+      <ClassComp initNumber={2} />
     </div>
   );
 }
---
+++ src/ClassComp.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+let classStyle = "color:red";
+class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber,
+    date: new Date().toString(),
+  };
+
+  componentWillMount() {
+    console.log("%cclass => componentWillMount", classStyle);
+  }
+
+  componentDidMount() {
+    console.log("%cclass => componentDidMount", classStyle);
+  }
+  render() {
+    console.log("%cclass => render", classStyle);
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input
+          type="button"
+          value="randomNumber"
+          onClick={() => {
+            this.setState({
+              number: Math.random(),
+            });
+          }}
+        ></input>
+        <input
+          type="button"
+          value="randomDate"
+          onClick={() => {
+            this.setState({
+              date: new Date().toString(),
+            });
+          }}
+        ></input>
+      </div>
+    );
+  }
+}
+export default ClassComp;
